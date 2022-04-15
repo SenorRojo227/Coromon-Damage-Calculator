7e7ad41db60fe5d73b4391140593ce6b40dce25e
SrRojo <wgaliber@gmail.com>
2022-04-15 00:44:18 -0400

    Full Release

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let coromon = getCoromon();
-let skills = getSkills();
-let traits = getTraits();
-let items = getItems();
+import coromonJSON from "./src/coromon.json" assert {type: 'json'};
+import skillsJSON from "./src/skills.json" assert {type: 'json'};
+import traitsJSON from "./src/traits.json" assert {type: 'json'};
+import itemsJSON from "./src/items.json" assert {type: 'json'};
+import setsJSON from "./src/sets.json" assert {type: 'json'};
 
-let chosenMons = [coromon[0].statBoost = [0, 0, 0, 0, 0], coromon[0].statBoost = [0, 0, 0, 0, 0]];
-let chosenSkills = [[skills[0].crit = false, skills[0].crit = false, skills[0].crit = false, skills[0].crit = false],
-                    [skills[0].crit = false, skills[0].crit = false, skills[0].crit = false, skills[0].crit = false]];
+const coromon = coromonJSON.S1;
+const skills = skillsJSON.S1;
+const traits = traitsJSON.S1;
+const items = itemsJSON.S1["Held Items"];
+
+let coromonList = Object.keys(coromon);
+let skillList = Object.keys(skills);
+
+let chosenMons = [coromon.Cubzero.statBoost = [0, 0, 0, 0, 0], coromon.Cubzero.statBoost = [0, 0, 0, 0, 0]];
+let chosenSkills = [[skills["(No Skill)"].crit = false, skills["(No Skill)"].crit = false, skills["(No Skill)"].crit = false, skills["(No Skill)"].crit = false],
+                    [skills["(No Skill)"].crit = false, skills["(No Skill)"].crit = false, skills["(No Skill)"].crit = false, skills["(No Skill)"].crit = false]];
 
 window.onload = function() {
     initCoromon();
@@ -19,16 +28,17 @@ window.onload = function() {
 
 function initCoromon() {
     for (let i = 1; i <= 2; i++) {
-        for (let j = 0; j < coromon.length; j++) {
+        for (let j = 0; j < coromonList.length; j++) {
             let optgroup = document.createElement("optgroup");
-            optgroup.label = coromon[j].name;
+            optgroup.label = coromonList[j];
+            /*
             for (let k = 0; k < coromon[j].sets.length; k++) {
                 let option = document.createElement("option");
                 option.innerHTML = coromon[j].name + " (" + coromon[j].sets[k].setTitle + ")";
                 optgroup.appendChild(option);
-            }
+            }*/
             let blankOption = document.createElement("option");
-            blankOption.innerHTML = coromon[j].name + " (Blank Set)";
+            blankOption.innerHTML = coromonList[j] + " (Blank Set)";
             optgroup.appendChild(blankOption);
 
             document.getElementById("name" + i).appendChild(optgroup);
@@ -41,9 +51,9 @@ function initSkills() {
     let runTimes = 0;
     while (runTimes < 2) {
         for (let i = 1; i <= 4; i++) {
-            for (let j = 0; j < skills.length; j++) {
+            for (let j = 0; j < skillList.length; j++) {
                 let option = document.createElement("option");
-                option.innerHTML = skills[j].name;
+                option.innerHTML = skillList[j];
                 document.getElementById("skillInput" + side + i).appendChild(option);
             }
         }
@@ -54,9 +64,9 @@ function initSkills() {
 
 function initTraits() {
     for (let i = 1; i <= 2; i++) {
-        for (let j = 0; j < traits.length; j++) {
+        for (let j = 0; j < Object.keys(traits).length; j++) {
             let option = document.createElement("option");
-            option.innerHTML = traits[j];
+            option.innerHTML = Object.keys(traits)[j];
             document.getElementById("trait" + i).appendChild(option);
         }
     }
@@ -64,9 +74,9 @@ function initTraits() {
 
 function initItems() {
     for (let i = 1; i <= 2; i++) {
-        for (let j = 0; j < items.length; j++) {
+        for (let j = 0; j < Object.keys(items).length; j++) {
             let option = document.createElement("option");
-            option.innerHTML = items[j];
+            option.innerHTML = Object.keys(items)[j];
             document.getElementById("item" + i).appendChild(option);
         }
     }
@@ -75,61 +85,61 @@ function initItems() {
 function resetSet(id) {
 
     //Reset Lists
-    coromon = getCoromon();
-    skills = getSkills();
+    //coromon = coromonJSON.S1;
+    //skills = skillsJSON.S1;
 
     //Coromon Search
     let selectedCoromon = document.querySelector("#name" + id + " option:checked").parentElement.label;
-    let crmIndex = findCoromon(selectedCoromon);
-    chosenMons[id - 1] = coromon[crmIndex];
+    chosenMons[id - 1] = coromon[selectedCoromon];
+    chosenMons[id - 1].name = selectedCoromon;
     chosenMons[id - 1].statBoost = [0, 0, 0, 0, 0];
+    chosenMons[id - 1].sets = [];
 
     chosenMons[id - 1].sets.push(
         {
             setTitle: "Blank Set",
-            level: 25,
+            level: 50,
             potential: [0, 0, 0, 0, 0, 0, 0],
-            trait: "(none)",
-            item: "(none)",
+            trait: "(No Trait)",
+            item: "(No Item)",
             skills: ["(No Skill)", "(No Skill)", "(No Skill)", "(No Skill)"]
         }
     );
 
     //Skill Search
-    let skillIndex = [];
     for (let i = 0; i < 4; i++) {
-        skillIndex[i] = findSkill(chosenMons[id - 1].sets[0].skills[i]);
-        chosenSkills[id - 1][i] = skills[skillIndex[i]];
+        chosenSkills[id - 1][i] = skills[chosenMons[id - 1].sets[0].skills[i]];
+        chosenSkills[id - 1][i].name = chosenMons[id - 1].sets[0].skills[i];
     }
 
     //Reset Name
-    document.getElementById("resultHeader" + id).innerHTML = coromon[crmIndex].name + "'s skills (Select one for more info)";
+    document.getElementById("resultHeader" + id).innerHTML = selectedCoromon + "'s skills (Select one for more info)";
     
     //Reset Type
-    document.getElementById("type" + id).value = coromon[crmIndex].type;
+    document.getElementById("type" + id).value = coromon[selectedCoromon].Type;
 
     //Reset Level
-    document.getElementById("lvl" + id).value = 25;
+    document.getElementById("lvl" + id).value = 50;
 
     //Reset Stats
-    document.getElementById("hp" + id).value = coromon[crmIndex].stats[0];
-    document.getElementById("sp" + id).value = coromon[crmIndex].stats[1];
-    document.getElementById("atk" + id).value = coromon[crmIndex].stats[2];
-    document.getElementById("def" + id).value = coromon[crmIndex].stats[3];
-    document.getElementById("spAtk" + id).value = coromon[crmIndex].stats[4];
-    document.getElementById("spDef" + id).value = coromon[crmIndex].stats[5];
-    document.getElementById("spe" + id).value = coromon[crmIndex].stats[6];
+    document.getElementById("hp" + id).value = coromon[selectedCoromon].Stats["HP"];
+    document.getElementById("sp" + id).value = coromon[selectedCoromon].Stats["SP"];
+    document.getElementById("atk" + id).value = coromon[selectedCoromon].Stats["Atk"];
+    document.getElementById("def" + id).value = coromon[selectedCoromon].Stats["Def"];
+    document.getElementById("spAtk" + id).value = coromon[selectedCoromon].Stats["Sp.A"];
+    document.getElementById("spDef" + id).value = coromon[selectedCoromon].Stats["Sp.D"];
+    document.getElementById("spe" + id).value = coromon[selectedCoromon].Stats["Spe"];
 
     //Reset Trait
-    document.getElementById("trait" + id).value = coromon[crmIndex].sets[0].trait;
+    document.getElementById("trait" + id).value = "(No Trait)";       //!!!Fix
     if (document.getElementById("trait" + id).value == null) {
-        document.getElementById("trait" + id).value == "(none)";
+        document.getElementById("trait" + id).value == "(No Trait)";
     }
 
     //Reset Item
-    document.getElementById("item" + id).value = coromon[crmIndex].sets[0].item;
+    document.getElementById("item" + id).value = "(No Item)";           //!!!Fix
     if (document.getElementById("item" + id).value == null) {
-        document.getElementById("item" + id).value == "(none)";
+        document.getElementById("item" + id).value == "(No Item)";
     }
 
     let side;
@@ -140,7 +150,7 @@ function resetSet(id) {
     }
 
     //Blank Set Bug
-    //let setIndex = Array.prototype.crmIndexOf.call(document.querySelector("#name" + id + " option:checked").parentElement,
+    //let setIndex = Array.prototype.selectedCoromonOf.call(document.querySelector("#name" + id + " option:checked").parentElement,
     //document.querySelector("#name" + id + " option:checked"));
     //console.log(setIndex);
     
@@ -157,11 +167,11 @@ function resetSet(id) {
 
     //Reset Skill Info
     for (let i = 1; i <= 4; i++) {
-        document.getElementById("skillLabel" + side + i).innerHTML = chosenSkills[id - 1][i - 1].name;
-        document.getElementById("skillInput" + side + i).value = chosenSkills[id - 1][i - 1].name;
-        document.getElementById("skillDmg" + side + i).value = chosenSkills[id - 1][i - 1].power;
-        document.getElementById("skillType" + side + i).value = chosenSkills[id - 1][i - 1].type;
-        document.getElementById("skillDmgType" + side + i).value = chosenSkills[id - 1][i - 1].atkType;
+        document.getElementById("skillLabel" + side + i).innerHTML = chosenMons[id - 1].sets[0].skills[i - 1];
+        document.getElementById("skillInput" + side + i).value = chosenMons[id - 1].sets[0].skills[i - 1];
+        document.getElementById("skillDmg" + side + i).value = chosenSkills[id - 1][i - 1].Power;
+        document.getElementById("skillType" + side + i).value = chosenSkills[id - 1][i - 1].Type;
+        document.getElementById("skillDmgType" + side + i).value = chosenSkills[id - 1][i - 1].Category;
     }
 
     //Reset Crit
@@ -183,28 +193,29 @@ function updateSet(id) {
     }
 
     //Update Type
-    chosenMons[id - 1].type = document.getElementById("type" + id).value;
+    chosenMons[id - 1].Type = document.getElementById("type" + id).value;
 
     //Update Level
     chosenMons[id - 1].sets[0].level = document.getElementById("lvl" + id).value;
 
     //Update Stats
-    chosenMons[id - 1].stats[0] = document.getElementById("hp" + id).value;
-    chosenMons[id - 1].stats[1] = document.getElementById("sp" + id).value;
-    chosenMons[id - 1].stats[2] = document.getElementById("atk" + id).value;
-    chosenMons[id - 1].stats[3] = document.getElementById("def" + id).value;
-    chosenMons[id - 1].stats[4] = document.getElementById("spAtk" + id).value;
-    chosenMons[id - 1].stats[5] = document.getElementById("spDef" + id).value;
-    chosenMons[id - 1].stats[6] = document.getElementById("spe" + id).value;
+    chosenMons[id - 1].Stats[0] = document.getElementById("hp" + id).value;
+    chosenMons[id - 1].Stats[1] = document.getElementById("sp" + id).value;
+    chosenMons[id - 1].Stats[2] = document.getElementById("atk" + id).value;
+    chosenMons[id - 1].Stats[3] = document.getElementById("def" + id).value;
+    chosenMons[id - 1].Stats[4] = document.getElementById("spAtk" + id).value;
+    chosenMons[id - 1].Stats[5] = document.getElementById("spDef" + id).value;
+    chosenMons[id - 1].Stats[6] = document.getElementById("spe" + id).value;
     for (let i = 1; i <= 5; i++) {
         chosenMons[id - 1].statBoost[i - 1] = parseInt(document.getElementById("statBoost" + side + i).value);
     }
 
     for (let i = 1; i <= 7; i++) {
         chosenMons[id - 1].sets[0].potential[i - 1] = parseInt(document.getElementById("potInput" + side + i).value);
-        baseStat = chosenMons[id - 1].stats[i - 1];
-        level = chosenMons[id - 1].sets[0].level;
-        potential = chosenMons[id - 1].sets[0].potential[i - 1];
+        let baseStat = chosenMons[id - 1].Stats[Object.keys(chosenMons[id - 1].Stats)[i - 1]];
+        let level = chosenMons[id - 1].sets[0].level;
+        let potential = chosenMons[id - 1].sets[0].potential[i - 1];
+        let boost;
         if (i >= 3) {
             boost = chosenMons[id - 1].statBoost[i - 3];
             if (boost > 0) {
@@ -249,12 +260,22 @@ function updateSet(id) {
 
     //Update Skills
     for (let i = 1; i <= 4; i++) {
-        chosenMons[id - 1].sets[0].skills[i - 1] = document.getElementById("skillInput" + side + i).value;
-        document.getElementById("skillLabel" + side + i).innerHTML = chosenMons[id - 1].sets[0].skills[i - 1];
-        chosenSkills[id - 1][i - 1] = skills[findSkill(chosenMons[id - 1].sets[0].skills[i - 1])];
-        chosenSkills[id - 1][i - 1].power = document.getElementById("skillDmg" + side + i).value;
-        chosenSkills[id - 1][i - 1].type = document.getElementById("skillType" + side + i).value;
-        chosenSkills[id - 1][i - 1].atkType = document.getElementById("skillDmgType" + side + i).value;
+        if (chosenMons[id - 1].sets[0].skills[i - 1] != document.getElementById("skillInput" + side + i).value) {
+            chosenMons[id - 1].sets[0].skills[i - 1] = document.getElementById("skillInput" + side + i).value;
+            document.getElementById("skillLabel" + side + i).innerHTML = chosenMons[id - 1].sets[0].skills[i - 1];
+            chosenSkills[id - 1][i - 1] = skills[chosenMons[id - 1].sets[0].skills[i - 1]];
+            document.getElementById("skillDmg" + side + i).value = chosenSkills[id - 1][i - 1].Power;
+            document.getElementById("skillType" + side + i).value = chosenSkills[id - 1][i - 1].Type;
+            document.getElementById("skillDmgType" + side + i).value = chosenSkills[id - 1][i - 1].Category;
+            chosenSkills[id - 1][i - 1].crit = false;
+            document.getElementById("critLabel" + side + i).classList.remove("checked");
+        } else {
+            chosenSkills[id - 1][i - 1] = skills[chosenMons[id - 1].sets[0].skills[i - 1]];
+            chosenSkills[id - 1][i - 1].Power = document.getElementById("skillDmg" + side + i).value;
+            chosenSkills[id - 1][i - 1].Type = document.getElementById("skillType" + side + i).value;
+            chosenSkills[id - 1][i - 1].Category = document.getElementById("skillDmgType" + side + i).value;
+        }
+        chosenSkills[id - 1][i - 1].name = document.getElementById("skillInput" + side + i).value;
     }
 
     //Update Trait
@@ -306,18 +327,6 @@ function calculate() {
     }
 }
 
-function findCoromon(name) {
-    for (let i = 0; i < coromon.length; i++) {
-        if (name == coromon[i].name) {
-            return i;
-        }
-    }
-}
-
-function findSkill(name) {
-    for (let i = 0; i < skills.length; i++) {
-        if (name == skills[i].name) {
-            return i;
-        }
-    }
-}
+window.resetSet = resetSet;
+window.updateSet = updateSet;
+window.calculate = calculate;
--- src/calc.js
+++ src/calc.js
@@ -12,6 +12,8 @@ function getDamage(atkMon, defMon, selectedSkill, weather = "none") {
     let feelers = 1;
     let boost1 = 0;
     let boost2 = 0;
+    let boostLevel1 = 0;
+    let boostLevel2 = 0;
     let weatherBonus = 1;
 
     //Check Inputs
@@ -27,29 +29,29 @@ function getDamage(atkMon, defMon, selectedSkill, weather = "none") {
       feelers = 1;
     }
 
-    if (selectedSkill.atkType == "Physical") {
-        A = Math.floor(atkMon.stats[2] * atkMon.sets[0].level / 99) + 5 + atkMon.sets[0].potential[2];
-        D = Math.floor(defMon.stats[3] * defMon.sets[0].level / 99) + 5 + defMon.sets[0].potential[3];
-        boost1 = atkMon.statBoost[0];
-        boost2 = defMon.statBoost[1];
-    } else if (selectedSkill.atkType == "Special") {
-        A = Math.floor(atkMon.stats[4] * atkMon.sets[0].level / 99) + 5 + atkMon.sets[0].potential[4];
-        D = Math.floor(defMon.stats[5] * defMon.sets[0].level / 99) + 5 + defMon.sets[0].potential[5];
-        boost1 = atkMon.statBoost[2];
-        boost2 = defMon.statBoost[3];
+    if (selectedSkill.Category == "Physical") {
+        A = Math.floor(atkMon.Stats["Atk"] * atkMon.sets[0].level / 99) + 5 + atkMon.sets[0].potential[2];
+        D = Math.floor(defMon.Stats["Sp.A"] * defMon.sets[0].level / 99) + 5 + defMon.sets[0].potential[3];
+        boostLevel1 = atkMon.statBoost[0];
+        boostLevel2 = defMon.statBoost[1];
+    } else if (selectedSkill.Category == "Special") {
+        A = Math.floor(atkMon.Stats["Sp.A"] * atkMon.sets[0].level / 99) + 5 + atkMon.sets[0].potential[4];
+        D = Math.floor(defMon.Stats["Sp.D"] * defMon.sets[0].level / 99) + 5 + defMon.sets[0].potential[5];
+        boostLevel1 = atkMon.statBoost[2];
+        boostLevel2 = defMon.statBoost[3];
     } else {
         return [0, 0, 0, 0]
     }
 
-    defHP = Math.floor(defMon.stats[0] * defMon.sets[0].level / 99) + 10 + parseInt(defMon.sets[0].level) + parseInt(defMon.sets[0].potential[0]);
+    defHP = Math.floor(defMon.Stats["HP"] * defMon.sets[0].level / 99) + 10 + parseInt(defMon.sets[0].level) + parseInt(defMon.sets[0].potential[0]);
 
-    if (atkMon.type == selectedSkill.type) {
+    if (atkMon.Type == selectedSkill.Type) {
       stab = 1.25;
     } else {
       stab = 1;
     }
     
-    effectiveness = getEffectiveness(selectedSkill.type, defMon.type);
+    effectiveness = getEffectiveness(selectedSkill.Type, defMon.Type);
     
     if (atkMon.sets[0].trait == "Clumsy Power") {
       trait1 = 1.25;
@@ -63,10 +65,10 @@ function getDamage(atkMon, defMon, selectedSkill, weather = "none") {
       trait2 = 0.6;
     }
 
-    if (boost1 > 0) {
-      boost1 = 1 + boost1 * 0.5;
-    } else if (boost1 < 0) {
-      switch(boost1) {
+    if (boostLevel1 > 0) {
+      boost1 = 1 + boostLevel1 * 0.5;
+    } else if (boostLevel1 < 0) {
+      switch(boostLevel1) {
         case -1:
           boost1 = 0.66;
           break;
@@ -90,10 +92,10 @@ function getDamage(atkMon, defMon, selectedSkill, weather = "none") {
       boost1 = 1;
     }
 
-    if (boost2 > 0) {
-      boost2 = 1 + boost2 * 0.5;
-    } else if (boost2 < 0) {
-      switch(boost2) {
+    if (boostLevel2 > 0) {
+      boost2 = 1 + boostLevel2 * 0.5;
+    } else if (boostLevel2 < 0) {
+      switch(boostLevel2) {
         case -1:
           boost2 = 0.66;
           break;
@@ -118,13 +120,7 @@ function getDamage(atkMon, defMon, selectedSkill, weather = "none") {
     }
 
     //Check Weather
-    if (weather == "Rain" && selectedSkill.type == "Water") {
-        weatherBonus = 1.5;
-    }
-    if (weather == "Sandstorm" && selectedSkill.type == "Sand") {
-        weatherBonus = 1.5;
-    }
-    if (weather == "Snow" && selectedSkill.type == "Ice") {
+    if (weather == "Rain" && selectedSkill.Type == "Water") {
         weatherBonus = 1.5;
     }
 
@@ -135,8 +131,8 @@ function getDamage(atkMon, defMon, selectedSkill, weather = "none") {
         var highDamage = lowDamage;
     } else {
         var multipliers = stab * trait1 * trait2 * weatherBonus * crit * feelers * effectiveness * boost1 / boost2;
-        var lowDamage = Math.floor( ((((((2*atkMon.sets[0].level)/5) + 2) * selectedSkill.power * A / D) / 50) + 2) * multipliers * 0.85 );
-        var highDamage = Math.floor( ((((((2*atkMon.sets[0].level)/5) + 2) * selectedSkill.power * A / D) / 50) + 2) * multipliers );
+        var lowDamage = Math.floor( ((((((2*atkMon.sets[0].level)/5) + 2) * selectedSkill.Power * A / D) / 50) + 2) * multipliers * 0.85 );
+        var highDamage = Math.floor( ((((((2*atkMon.sets[0].level)/5) + 2) * selectedSkill.Power * A / D) / 50) + 2) * multipliers );
     }
     var lowPerc = Math.round((lowDamage / defHP) * 100);
     var highPerc = Math.round((highDamage / defHP) * 100);
@@ -147,18 +143,51 @@ function getDamage(atkMon, defMon, selectedSkill, weather = "none") {
 
 function getStatement(atkMon, defMon, selectedSkill) {
     let statement = "";
-    if (selectedSkill.atkType == "Physical") {
+
+    if (selectedSkill.Category == "Physical") {
+
+        if (atkMon.statBoost[0] > 0) {
+            statement += "+" + atkMon.statBoost[0] + " ";
+        } else if (atkMon.statBoost[0] < 0) {
+            statement += atkMon.statBoost[0] + " ";
+        }
+
         statement += atkMon.sets[0].potential[2] + " Atk ";
-    } else if (selectedSkill.atkType == "Special") {
+
+    } else if (selectedSkill.Category == "Special") {
+
+        if (atkMon.statBoost[2] > 0) {
+            statement += "+" + atkMon.statBoost[2] + " ";
+        } else if (atkMon.statBoost[2] < 0) {
+            statement += atkMon.statBoost[2] + " ";
+        }
+
         statement += atkMon.sets[0].potential[4] + " SpA ";
+
     }
 
     statement += atkMon.name + " " + selectedSkill.name + " vs. ";
 
-    if (selectedSkill.atkType == "Physical") {
+    if (selectedSkill.Category == "Physical") {
+
+        if (defMon.statBoost[1] > 0) {
+            statement += "+" + defMon.statBoost[1] + " ";
+        } else if (defMon.statBoost[1] < 0) {
+            statement += defMon.statBoost[1] + " ";
+        }
+
         statement += defMon.sets[0].potential[3] + " Def ";
-    } else if (selectedSkill.atkType == "Special") {
+
+    } else if (selectedSkill.Category == "Special") {
+
+        if (defMon.statBoost[3] > 0) {
+            statement += "+" + defMon.statBoost[3] + " ";
+        } else if (defMon.statBoost[3] < 0) {
+            statement += defMon.statBoost[3] + " ";
+        }
+
         statement += defMon.sets[0].potential[5] + " SpD ";
+
     }
 
     statement += defMon.name + ": " + getDamage(atkMon, defMon, selectedSkill)[0] + "-"
